18bd13eaa79779eb16a531dd54cb87f82a6a2bd6
Murilo Silva Moraes <murilosilvamoraes@gmail.com>
2021-04-12 20:13:19 -0300

    front-final

--- src/app/ver-saques/ver-saques.page.ts
+++ src/app/ver-saques/ver-saques.page.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerSaquesPage implements OnInit {
 
+  escolher = true
+  pendentes = false
+  aprovados = false
+  cancelados = false
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showEscolher(){
+    this.escolher = true;
+    this.pendentes = false;
+    this.aprovados = false;
+    this.cancelados = false;
+  }
+
+  showPendentes(){
+    this.escolher = false;
+    this.pendentes = true;
+    this.aprovados = false;
+    this.cancelados = false;
+  }
+
+  showAprovados(){
+    this.escolher = false;
+    this.pendentes = false;
+    this.aprovados = true;
+    this.cancelados = false;
+  }
+
+  showCancelados(){
+    this.escolher = false;
+    this.pendentes = false;
+    this.aprovados = false;
+    this.cancelados = true;
   }
 
 }
